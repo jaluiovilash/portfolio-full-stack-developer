ba809297a17914a55e0a62c8460387e1b7d7eb3a
jaluiovilash <ovilashjalui@gmail.com>
2024-05-13 11:44:14 +0530

    adding the link of my repo in github button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
+// form submission code - display
+
+document.getElementById("contactForm").addEventListener("submit", function (event) {
     event.preventDefault();
-    // You can add your code here to handle form submission, like sending an email or saving to a database.
     alert("Form submitted successfully!");
     document.getElementById("contactForm").reset();
 });
@@ -11,9 +12,17 @@ function toggleAnswer(id) {
     var icon = document.getElementById("icon" + id.slice(-1));
     if (answer.style.display === "none") {
         answer.style.display = "block";
-        // icon.textContent = ""; // Change icon to minus sign
     } else {
         answer.style.display = "none";
-        // icon.textContent = ""; // Change icon back to plus sign
     }
 }
+
+// clicking the button - github, to open my repos
+
+document.getElementById("btn-github").addEventListener("click", function () {
+    var link = document.createElement("a");
+    link.href = "https://github.com/jaluiovilash?tab=repositories";
+    link.target = "_blank";
+    document.body.appendChild(link);
+    link.click();
+});
